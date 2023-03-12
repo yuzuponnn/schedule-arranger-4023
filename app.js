53d6f26040cf79eeacd93665ddc4540b3338a569
Nobira Yuzuki <yuzuman1422@gmail.com>
2023-03-13 00:19:29 +0900

    renderの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
-const GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '1847d74373adb2e09584';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'a04b41fa2873450cd57de5e26f6966fb7c70c96a';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -39,7 +39,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.CALLBACK_URL || 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(async function () {
